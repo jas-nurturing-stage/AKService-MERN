67198e92e904261233fa929394586b1409a87cb6
Jasmine Shaik <sk.jasminesaheb@gmail.com>
2025-09-28 14:01:15 +0530

    updated backend and helm charts

--- backend/helloService/index.js
+++ backend/helloService/index.js
@@ -9,7 +9,7 @@ app.use(cors())
 
 app.use(express.json());
 app.get('/', (req,res)=>{
-    res.send({msg: 'Hello World'})
+    res.send({msg: 'Hello JASMINE'})
 })
 app.get('/health', (req,res)=>{
     res.send({status: 'OK'})
